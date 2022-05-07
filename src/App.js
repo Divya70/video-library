476da1cba48a1e7af4aa71da77f3eeb457b98014
Divya70 <divyanamdev8795@gmail.com>
2022-05-07 11:18:21 +0530

    resolve merge conflit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-<<<<<<< HEAD
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
@@ -14,10 +13,6 @@ function App() {
       </Routes>
     </div>
   );
-=======
-function App() {
-  return <div className="App"></div>;
->>>>>>> fdde2d86e507b64edddb00893adec0478bc1c31c
 }
 
 export default App;
